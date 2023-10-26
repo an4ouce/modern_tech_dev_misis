c590d4eee46d4e85bbd0b152b0dda356987bbe00
Anna Izotova <82799382+an4ouce@users.noreply.github.com>
2023-10-26 16:15:57 +0300

    Add files via upload
    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 '''
 Password Checker
--------------------------------------------------------------
 '''
 import secrets
 import string
@@ -18,13 +17,20 @@ def check_pwd():
         pwd = input("Введите пароль для проверки: ")
         if len(pwd) < 8:
             print("Пароль слишком короткий!")
-        if not (any(char in digits for char in pwd)):
+            print("-----------------------------------------------")
+        elif (all(char in special_chars for char in pwd)):
+            print("Пароль не может состоять только из спецсимволов!")
+            print("-----------------------------------------------")
+        elif not (any(char in digits for char in pwd)):
             print("Пароль не может состоять только из букв!")
-        if not (any(char in letters for char in pwd)):
+            print("-----------------------------------------------")
+        elif not (any(char in letters for char in pwd)):
             print("Пароль не может состоять только из цифр!")
-        if not (any(char in letters[26:] for char in pwd)):
+            print("-----------------------------------------------")
+        elif not (any(char in letters[26:] for char in pwd)):
             print("В пароле хотя бы одна буква должна быть в верхнем регистре!")  
-        if not (any(char in special_chars for char in pwd)):
+            print("-----------------------------------------------")
+        elif not (any(char in special_chars for char in pwd)):
             print("В пароле отсутствует спецсимвол!")
             print("-----------------------------------------------")
             n+=1
